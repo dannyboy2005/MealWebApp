8ce84f3d451a1c3ccd27ca88a1bfd7814b21d025
Daniel Boffey <dboffey70@googlemail.com>
2020-07-09 15:56:23 +0100

    search view updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import _ from "lodash";
 import "./style.css";
 import Search from "./models/Search";
+import * as searchView from "./view/searchView";
+import { elements } from "./view/base";
 
 // Global state of the app
 // --Search object
@@ -11,23 +13,25 @@ const state = {};
 
 const controlSearch = async () => {
   // Get query from view
-  const query = "pizza"; // Placeholder
+  const query = searchView.getInput(); // Placeholder
 
   if (query) {
     // New search object and add to state
     state.search = new Search(query);
 
     // Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
 
     // Search for recipes
     await state.search.getResults();
 
     // render results on the UI
-    console.log(state.search.result);
+    searchView.renderResults(state.search.result);
   }
 };
 
-document.querySelector(".search").addEventListener("submit", (e) => {
+elements.searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   controlSearch();
 });
